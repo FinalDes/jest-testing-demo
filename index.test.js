b778aaa8bb624c5954133fdccaccdf26170e5905
Alex Alonso <alex4551@gmail.com>
2017-07-18 23:17:14 +0800

    use jest test runner

--- index.test.js
+++ index.test.js
@@ -1,91 +1,79 @@
-const { expect, should } = require('chai');
-
-should();
-
 const {
     alwaysTrue,
     legitString,
 } = require('./index');
 
-describe('#mocha bascis', () => {
-  it('true should be true', () => {
-    true.should.be.true;
-  });
+describe('#jest bascis', () => {
   it('I expect true to be true', () => {
-    expect(true).to.be.true;
+    expect(true).toBe(true);
   });
 });
 
 describe('#alwaysTrue', () => {
-  it('should always return true', () => {
-    alwaysTrue().should.be.true;
-  });
-
   it('I expect it to always be true', () => {
-    expect(alwaysTrue()).to.be.true;
+    expect(alwaysTrue()).toBe(true);
   });
 
-  it('should not be false', () => {
-    alwaysTrue().should.not.be.false;
+  it('I expect it not to be false', () => {
+    expect(alwaysTrue()).not.toBe(false);
   });
 });
 
 describe('#legitString', () => {
   it("should detect 'cow' as legit string", () => {
-    legitString('cow').should.be.true;
+    expect(legitString('cow')).toBe(true);
   });
 
   it("empty string is not legit string", () => {
-    legitString('').should.be.true;
+    expect(legitString('')).toBe(true);
   });
 
   it('7 is not legit string', () => {
-    legitString(7).should.be.false;
+    expect(legitString(7)).toBe(false);
   });
 
   it("'7' is legit string", () => {
-    legitString('7').should.be.true;
+    expect(legitString('7')).toBe(true);
   });
 
   it('undefined is not legit string', () => {
-    legitString(undefined).should.be.false;
+    expect(legitString(undefined)).toBe(false);
   });
 
   it("'undefined' is a legit string",()=>{
-    legitString('undefined').should.be.true;
+    expect(legitString('undefined')).toBe(true);
   })
 
   it('null is not legit string', () => {
-    legitString(null).should.be.false;
+    expect(legitString(null)).toBe(false);
   });
 
-    it("'null' is a legit string",()=>{
-    legitString('null').should.be.true;
+  it("'null' is a legit string",()=>{
+    expect(legitString('null')).toBe(true);
   })
 
   it("true is not legit string",() => {
-    legitString(true).should.be.false;
+    expect(legitString(true)).toBe(false);
   });
 
   it("'true' is legit string",() => {
-    legitString("true").should.be.true;
+    expect(legitString("true")).toBe(true);
   });
 
   it("false is not legit string",() => {
-    legitString(false).should.be.false;
+    expect(legitString(false)).toBe(false);
   });
 
   it("'false' is legit string",() => {
-    legitString("false").should.be.true;
+    expect(legitString("false")).toBe(true);
   });
 
   it("empty object is not legit string",() => {
-    legitString({}).should.be.false;
+    expect(legitString({})).toBe(false);
   });
 
   it("'{}' is not legit string",() => {
-    legitString('{}').should.be.true;
+    expect(legitString('{}')).toBe(true);
   });
 
 });
-
